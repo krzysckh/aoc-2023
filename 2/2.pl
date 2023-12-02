5a5b450aa96efbf7da2a9ab418297527cab98319
krzysckh <krzysckh@wp.pl>
2023-12-02 14:36:19 +0100

    2

---
+++ 2/2.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use List::Util qw(max);
+
+use feature 'say';
+
+open my $f, '<', "./in" or die $@;
+
+sub p1 {
+  my ($p2) = @_;
+  my $sum = 0;
+  while (<$f>) {
+    my %max = (red => 0, green => 0, blue => 0);
+    my $id = [/^Game (\d+)/g]->[0];
+    s/^Game .*: //;
+
+    $max{$_->[1]} = max($max{$_->[1]}, $_->[0]) for map { [/(\d+)\s(.*)/g] } split /,|;\s?/;
+
+    if (not $p2) {
+      $sum += $id if $max{red} <= 12 && $max{green} <= 13 && $max{blue} <= 14;
+    } else {
+      $sum += $max{red} * $max{green} * $max{blue};
+    }
+  }
+
+  say $p2 ? "p2" : "p1", ": $sum"
+}
+
+sub p2 {
+  p1 1
+}
+
+p1;
+seek $f, 0, 0;
+p2;
+
+close $f;
