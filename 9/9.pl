2558258a9806f6e3806adb49da796b410e5e4497
krzysckh <krzysckh@wp.pl>
2023-12-09 13:31:48 +0100

    dry

--- 9/9.pl
+++ 9/9.pl
@@ -9,9 +9,11 @@ use List::Util qw(all zip reduce);
 open my $f, '<', "./in" or die $@;
 
 sub p1 {
+  my ($p2) = @_;
   my $sum = 0;
   while (<$f>) {
     my @n = split / /;
+    @n = reverse @n if $p2;
     my @lasts;
     while (1) {
       push @lasts, $n[-1];
@@ -28,22 +30,7 @@ sub p1 {
 }
 
 sub p2 {
-  my $sum = 0;
-  while (<$f>) {
-    my @n = split / /;
-    my @firsts;
-    while (1) {
-      push @firsts, $n[0];
-      my @pairs = zip [map { $_ } @n[1..@n-1]], \@n;
-      pop @pairs;
-      @n = map { $_->[1] - $_->[0] } @pairs;
-      last if all { $_ == 0 } @n;
-    }
-
-    $sum += reduce { $a + $b } @firsts;
-  }
-
-  say "p2: $sum";
+  p1 "yes yes yes";
 }
 
 p1;
